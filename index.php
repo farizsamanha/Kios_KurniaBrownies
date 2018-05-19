f0a81ce829975288a8a78231cc23a0d134b7f07f
farizsamanha <farizsamanha@gmail.com>
2018-05-19 19:04:04 +0700

    mmn

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>KIOS MALASNGODING</title>
+	<title>Kurnia Brownies</title>
 	<link rel="stylesheet" type="text/css" href="assets/css/bootstrap.css">
 	<link rel="stylesheet" type="text/css" href="assets/js/jquery-ui/jquery-ui.css">
 	<script type="text/javascript" src="assets/js/jquery.js"></script>
