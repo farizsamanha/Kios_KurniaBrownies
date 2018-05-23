3eddbda17c73e67f83d9bf69df7c098bc910ae9c
farizsamanha <farizsamanha@gmail.com>
2018-05-23 12:07:27 +0700

    xx

--- admin/editbahanbaku.php
+++ admin/editbahanbaku.php
@@ -10,17 +10,20 @@ while($d=mysql_fetch_array($det)){
 ?>					
 	<form action="updatebahanbaku.php" method="post">
 		<table class="table">
-			<tr>
+			<!-- <tr>
 				<td>ID</td>
-				<td><input type="text" name="id" value="<?php echo $d['id'] ?>" disabled></td>
+				<td><input type="text" name="id" value="<?php //echo $d['id'] ?>" disabled></td>
+			</tr> -->
+
+			<tr>
+				<td></td>
+				<td><input type="hidden" name="id" value="<?php echo $d['id'] ?>"></td>
 			</tr>
+
 			<tr>
 				<td>Nama</td>
 				<td><input type="text" class="form-control" name="nama" value="<?php echo $d['nama'] ?>"></td>
 			</tr>
-			
-			
-
 			<tr>
 				<td>Harga</td>
 				<td><input type="text" class="form-control" name="harga" value="<?php echo $d['harga'] ?>"></td>
@@ -29,22 +32,18 @@ while($d=mysql_fetch_array($det)){
 				<td>Jumlah</td>
 				<td><input type="text" class="form-control" name="jumlah" value="<?php echo $d['jumlah'] ?>"></td>
 			</tr>
-
-			<tr>
-				<td>sd</td>
-				<td><input type="text" class="form-control" name="sd" value="<?php echo $d['sd'] ?>"></td>
-			</tr>
-
 			<tr>
 				<td>lead</td>
 				<td><input type="text" class="form-control" name="lead" value="<?php echo $d['lead'] ?>"></td>
 			</tr>
-
+			<tr>
+				<td>sd</td>
+				<td><input type="text" class="form-control" name="sd" value="<?php echo $d['sd'] ?>"></td>
+			</tr>
 			<tr>
 				<td>sl</td>
 				<td><input type="text" class="form-control" name="sl" value="<?php echo $d['sl'] ?>"></td>
 			</tr>
-
 			<tr>
 				<td></td>
 				<td><input type="submit" class="btn btn-info" value="Simpan"></td>
--- admin/updatebahanbaku.php
+++ admin/updatebahanbaku.php
@@ -1,6 +1,6 @@
 <?php 
 include 'config.php';
-$id = $_POST['id'];
+$id=$_POST['id'];
 $nama=$_POST['nama'];
 
 $harga=$_POST['harga'];
@@ -9,11 +9,19 @@ $lead=$_POST['lead'];
 $sd=$_POST['sd'];
 $sl=$_POST['sl'];
 
-mysql_query("update bahanbaku set nama='$nama', harga='$harga', jumlah='$jumlah', lead='$lead', sd='$sd', sl='$sl' where id='$id'")or die(mysql_error());
+$x = mysql_query("update bahanbaku set nama='$nama', harga='$harga', jumlah='$jumlah', lead='$lead', sd='$sd', sl='$sl' where id='$id'")or die(mysql_error());
+
+// if($x){
+// 	echo '1';
+// }
+// else{
+// 	echo 'error';
+// } 
+
 header("location:rop.php");
 
 //'','$nama','$suplier','$harga','$jumlah','$lead','$sd','$sl', '', '', '', ''
 //update bahanbaku set nama='$nama', supplier='$suplier', harga='$harga', jumlah='$jumlah', lead='$lead', sd='$sd', sl='$sl' where id='$id'
-//update bahanbaku set nama='10', supplier='10', harga='10', jumlah='10', lead='10', sd='10', sl='10' where id='1'
+//update bahanbaku set nama='10', supplier='10', harga='10', jumlah='10', lead='10', sd='10', sl='10' where id='7'
 
 ?>
